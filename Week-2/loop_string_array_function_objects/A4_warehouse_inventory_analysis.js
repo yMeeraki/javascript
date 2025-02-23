3cecb01f6d320ca79c72cbbe063b278164c39197
Yukti Gupta <yuktigupta95@gmail.com>
2025-02-23 16:44:38 +0530

    Calculate total value of items in each category (quantity * price).

--- Week-2/loop_string_array_function_objects/A4_warehouse_inventory_analysis.js
+++ Week-2/loop_string_array_function_objects/A4_warehouse_inventory_analysis.js
@@ -2,3 +2,49 @@
 // Program to analyze warehouse inventory using a nested array structure.
 // Task 1: Calculate total value of items in each category (quantity * price).
 // Task 2: Find the most expensive product across all categories.
+
+
+const warehouse = [
+    ["Electronics", 
+        { name: "Laptop", quantity: 10, price: 50000 },
+        { name: "Phone", quantity: 30, price: 30000 },
+        { name: "Headphones", quantity: 50, price: 2000 }
+    ],
+    ["Furniture", 
+        { name: "Sofa", quantity: 5, price: 60000 },
+        { name: "Table", quantity: 10, price: 15000 },
+        { name: "Chair", quantity: 20, price: 5000 }
+    ],
+    ["Groceries", 
+        { name: "Rice", quantity: 100, price: 100 },
+        { name: "Wheat", quantity: 80, price: 120 },
+        { name: "Oil", quantity: 50, price: 200 }
+    ]
+];
+
+const inventory = {}
+
+
+console.log(getTotalValue(warehouse)); 
+// console.log(findMostExpensive(warehouse));
+
+
+
+function getTotalValue(warehouse) {
+    for (let i = 0; i < warehouse.length; i++){
+        const category = warehouse[i][0]
+        const items = warehouse[i].slice(1)
+        // console.log(category) 
+        // console.log(items)
+        inventory[category] = 0
+        for (let j = 0; j < items.length; j++){
+            inventory[category] += items[j].price * items[j].quantity
+        }      
+        
+    }
+    return inventory
+}
+
+// function findMostExpensive(warehouse){
+
+// }
